8479ff71d0c37eabfee82f98db7455b8c790b3ca
kcuyh <khj879123@gmail.com>
2020-02-10 19:16:58 +0900

    [update] chatting

--- lib/Chatting.dart
+++ lib/Chatting.dart
@@ -1,11 +1,74 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:nuka/Utils/rest_api_utils.dart';
+import 'package:web_socket_channel/web_socket_channel.dart';
+import 'package:http/http.dart' as http;
 
 class Chatting extends StatefulWidget {
+  int peerId, myid;
+  String peernickname,mynickname;
+  final WebSocketChannel channel;
+
+  Chatting({Key key, @required this.peerId, @required this.peernickname, @required this.myid, @required this.mynickname, @required this.channel}):super(key:key);
+
   @override
-  _ChattingState createState() => _ChattingState();
+  _ChattingState createState() => _ChattingState(peerId: peerId, peernickname: peernickname,myid: myid, mynickname: mynickname);
 }
 
 class _ChattingState extends State<Chatting> {
+
+  int peerId, myid;
+  String peernickname, mynickname;
+  List messages;
+  TextEditingController textEditingController = TextEditingController();
+
+  _ChattingState({Key key, @required this.peerId, @required this.peernickname, @required this.myid, @required this.mynickname});
+
+
+
+  Future GetMessage(int myid, int peerid) async{
+    http.Response response = await http.get(
+        Uri.encodeFull('${ServerIp}chat/api/messages/${myid}/${peerid}'),
+        headers: {"Accept": "application/json"});
+    var utf8convert= utf8.decode(response.bodyBytes);//한글화
+    return json.decode(utf8convert);
+  }
+
+  void _sendMessage(){
+
+    if (textEditingController.text.isNotEmpty) {
+      widget.channel.sink.add(json.encode({
+        'message': textEditingController.text,
+        'receiver' : peerId,
+        'sender' : myid
+      }));
+      textEditingController.clear();
+    }
+  }
+
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    messages = List();
+    GetMessage(myid, peerId).then((data){
+      setState(() {
+        messages = data;
+      });
+    });
+
+    widget.channel.stream.listen((data){
+      Map<String, dynamic> datamap = json.decode(data);
+      if(mounted){
+        setState(() {
+          messages.insert(0, datamap);
+        });
+      }
+    });
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,44 +87,17 @@ class _ChattingState extends State<Chatting> {
           color: Color.fromRGBO(236, 128, 130, 1.0),
         ),
         title: Text(
-          '뀨뀨 님과의 대화',
+          '${peernickname} 님과의 대화',
           style: TextStyle(
               fontSize: 20,
               color: Color.fromRGBO(236, 128, 130, 1),
               fontWeight: FontWeight.bold),
         ),
       ),
-      body: Container(
-        margin: EdgeInsets.symmetric(horizontal: 10),
-        child: ListView(
-          shrinkWrap: true,
-          children: <Widget>[
-            SizedBox(
-              height: 20,
-            ),
-            Row(
-              children: <Widget>[
-                CircleAvatar(
-                  child: Icon(Icons.person),
-                ),
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Text('뀨뀨'),
-                    Container(
-                      margin: EdgeInsets.only(top: 5.0),
-                      child: Text(
-                        '어디 살아?',
-                        style: TextStyle(fontSize: 15),
-                      ),
-                    ),
-                  ],
-                ),
-              ],
-            )
-          ],
-        ),
+      body: Column(
+        children: <Widget>[
+          buildListMessage(),
+        ],
       ),
       bottomNavigationBar: BottomAppBar(
         child: Row(
@@ -72,6 +108,7 @@ class _ChattingState extends State<Chatting> {
             Container(
               width: 300,
               child: TextField(
+                controller: textEditingController,
                 decoration: InputDecoration.collapsed(
                   hintText: '텍스트를 입력해주세요',
                 ),
@@ -81,7 +118,9 @@ class _ChattingState extends State<Chatting> {
               width: 30,
             ),
             IconButton(
-              onPressed: () {},
+              onPressed: () {
+                _sendMessage();
+              },
               icon: Icon(
                 Icons.send,
                 size: 30,
@@ -90,6 +129,44 @@ class _ChattingState extends State<Chatting> {
             ),
           ],
         ),
+      ),
+    );
+  }
+
+  Widget buildItem(var ds){
+    return (ds['sender'] == myid || ds['sender'] == mynickname)?Row(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: <Widget>[
+        Container(
+          child: Text(
+            (ds['message'] != null)? ds['message']: '',style: TextStyle(color: Colors.amber),
+          ),
+        ),
+      ],
+    ):Row(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: <Widget>[
+        Container(
+          child: Text(
+            (ds['message'] != null)? ds['message']: '',style: TextStyle(color: Colors.white),
+          ),
+        ),
+      ],
+    );
+  }
+
+  buildListMessage() {
+    if(messages == null){
+      return Container();
+    }
+    return Flexible(
+      child:ListView.builder(
+          padding: EdgeInsets.all(10),
+          itemCount: messages.length,
+          reverse: true,
+          itemBuilder:(context, index){
+            return buildItem(messages[index]);
+          }
       ),
     );
   }
