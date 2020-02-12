6e953b363a639e75e7b116f47b042b2ffb4f07af
kcuyh <khj879123@gmail.com>
2020-02-12 14:11:11 +0900

    [update] location

--- lib/BottomBar/BottomBarPart.dart
+++ lib/BottomBar/BottomBarPart.dart
@@ -1,10 +1,16 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:nuka/Utils/rest_api_utils.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import '../HomePage.dart';
 import '../ChatPage.dart';
 import '../ChannelPage.dart';
 import '../ProfilePage.dart';
 import '../StoryPage.dart';
 import 'package:curved_navigation_bar/curved_navigation_bar.dart';
+import 'package:http/http.dart' as http;
 
 class BottomBarPart extends StatefulWidget {
   @override
@@ -12,6 +18,56 @@ class BottomBarPart extends StatefulWidget {
 }
 
 class _BottomBarPartState extends State<BottomBarPart> {
+
+  Geolocator geolocator = Geolocator();
+
+  Future<Position> _getLocation() async {
+    var currentLocation;
+    try {
+      currentLocation = await geolocator.getCurrentPosition(
+          desiredAccuracy: LocationAccuracy.best);
+    } catch (e) {
+      currentLocation = null;
+    }
+    return currentLocation;
+  }
+
+  AddLocation() async {
+    _getLocation().then((position) {
+      AddPositions(position.longitude, position.latitude);
+    });
+  }
+
+  AddPositions(double longitude, double latitude) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setDouble('longitude', longitude);
+    prefs.setDouble('latitude', latitude);
+
+    final Map<String, dynamic> Data = {
+      "longitude": longitude,
+      "latitude": latitude};
+
+    String addr = '${ServerIp}auth/user/${prefs.getInt('id')}/';
+
+    var response = await http.patch(addr, headers: Header,body: json.encode(Data));
+
+
+
+    // 200 ok. 정상 동작임을 알려준다.
+
+    if(response.statusCode == 200){
+      var utf8convert= utf8.decode(response.bodyBytes);//한글화
+      Map data = json.decode(utf8convert);
+      print(data);
+    }else{
+      print(response.statusCode);
+      print(utf8.decode(response.bodyBytes));
+      //나중에 로그인 실패 메세지 토스트로 만들기
+      return null;
+    }
+  }
+
+
   final List<Widget> pages = [
     ChannelPage(
       key: PageStorageKey('Channel Page'),
@@ -62,6 +118,14 @@ class _BottomBarPartState extends State<BottomBarPart> {
               color: Colors.white,
             ),
           ]);
+
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    AddLocation();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
