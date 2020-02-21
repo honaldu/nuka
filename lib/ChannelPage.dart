54d294d2f70c519d3f7584e591fffb823802e986
heoseogho <hsh9636@gmail.com>
2020-02-21 15:14:19 +0900

    dd

--- lib/ChannelPage.dart
+++ lib/ChannelPage.dart
@@ -4,6 +4,7 @@ import 'ChannelContents/Near.dart';
 import 'ChannelContents/Rate.dart';
 import 'ShopPage.dart';
 import 'SizeConfig.dart';
+import 'package:transparent_image/transparent_image.dart';
 
 class ChannelPage extends StatefulWidget {
   const ChannelPage({Key key}) : super(key: key);
@@ -41,10 +42,11 @@ class _ChannelPageState extends State<ChannelPage> {
                           ),
                           child: Text(
                             '추천',
-                            style: TextStyle(color: Colors.black,fontSize: 8 * SizeConfig.widthMultiplier),
+                            style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 8 * SizeConfig.widthMultiplier),
                           ),
                         ),
-
                         Padding(
                           padding: EdgeInsets.fromLTRB(
                             0 * SizeConfig.widthMultiplier,
@@ -62,7 +64,7 @@ class _ChannelPageState extends State<ChannelPage> {
                             },
                             child: Icon(
                               Icons.shopping_cart,
-                              size: 10*SizeConfig.widthMultiplier,
+                              size: 10 * SizeConfig.widthMultiplier,
                               color: Colors.black,
                             ),
                           ),
@@ -78,68 +80,83 @@ class _ChannelPageState extends State<ChannelPage> {
                   mainAxisAlignment: MainAxisAlignment.center,
                   crossAxisAlignment: CrossAxisAlignment.center,
                   children: <Widget>[
-                    Expanded(
-                      flex: 1,
-                      child: Padding(
-                        padding: EdgeInsets.fromLTRB(
-                          1 * SizeConfig.widthMultiplier,
-                          1 * SizeConfig.heightMultiplier,
-                          1 * SizeConfig.widthMultiplier,
-                          1 * SizeConfig.heightMultiplier,
-                        ),
-                        child: Container(
-                          width: 10 * SizeConfig.widthMultiplier,
-                          height: 30 * SizeConfig.heightMultiplier,
-                          decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(8.0),
-                              border: Border.all(color: Colors.grey[300]),
-                              boxShadow: [
-                                BoxShadow(
-                                    color: Colors.grey[300],
-                                    blurRadius: 1.0,
-                                    spreadRadius: 1.0,
-                                    offset: Offset(3.0, 3.0))
-                              ]),
-                          child: ClipRRect(
-                            borderRadius: BorderRadius.circular(8.0),
-                            child: Image.asset(
-                              'Images/juhee0.jpg',
+                    Padding(
+                      padding: EdgeInsets.fromLTRB(
+                        1 * SizeConfig.widthMultiplier,
+                        1 * SizeConfig.heightMultiplier,
+                        1 * SizeConfig.widthMultiplier,
+                        1 * SizeConfig.heightMultiplier,
+                      ),
+                      child: Stack(
+                        children: <Widget>[
+                          Padding(
+                            padding: EdgeInsets.fromLTRB(
+                              3 * SizeConfig.widthMultiplier,
+                              3 * SizeConfig.heightMultiplier,
+                              3 * SizeConfig.widthMultiplier,
+                              3 * SizeConfig.heightMultiplier,
+                            ),
+                            child: CircularProgressIndicator(),
+                          ),
+                          ClipRRect(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(20)),
+                            child: FadeInImage(
+                              placeholder: MemoryImage(kTransparentImage),
+                              image: AssetImage(
+                                'Images/juhee1.jpg',
+                              ),
                               fit: BoxFit.fill,
+                              fadeInDuration: Duration(seconds: 1),
+                              fadeOutDuration: Duration(seconds: 1),
+                              fadeInCurve: Curves.easeIn,
+                              fadeOutCurve: Curves.easeIn,
+                              height: 40 * SizeConfig.widthMultiplier,
+                              width: 40 * SizeConfig.widthMultiplier,
+                              repeat: ImageRepeat.repeat,
                             ),
                           ),
-                        ),
+                        ],
                       ),
                     ),
-                    Expanded(
-                      flex: 1,
-                      child: Padding(
-                        padding: EdgeInsets.fromLTRB(
-                          1 * SizeConfig.widthMultiplier,
-                          1 * SizeConfig.heightMultiplier,
-                          1 * SizeConfig.widthMultiplier,
-                          1 * SizeConfig.heightMultiplier,
-                        ),
-                        child: Container(
-                          width: 10 * SizeConfig.widthMultiplier,
-                          height: 30 * SizeConfig.heightMultiplier,
-                          decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(8.0),
-                              border: Border.all(color: Colors.grey[300]),
-                              boxShadow: [
-                                BoxShadow(
-                                    color: Colors.grey[300],
-                                    blurRadius: 1.0,
-                                    spreadRadius: 1.0,
-                                    offset: Offset(3.0, 3.0))
-                              ]),
-                          child: ClipRRect(
-                            borderRadius: BorderRadius.circular(8.0),
-                            child: Image.asset(
-                              'Images/juhee1.jpg',
+                    SizedBox(width: 3 * SizeConfig.widthMultiplier,),
+                    Padding(
+                      padding: EdgeInsets.fromLTRB(
+                        1 * SizeConfig.widthMultiplier,
+                        1 * SizeConfig.heightMultiplier,
+                        1 * SizeConfig.widthMultiplier,
+                        1 * SizeConfig.heightMultiplier,
+                      ),
+                      child: Stack(
+                        children: <Widget>[
+                          Padding(
+                            padding: EdgeInsets.fromLTRB(
+                              3 * SizeConfig.widthMultiplier,
+                              3 * SizeConfig.heightMultiplier,
+                              3 * SizeConfig.widthMultiplier,
+                              3 * SizeConfig.heightMultiplier,
+                            ),
+                            child: CircularProgressIndicator(),
+                          ),
+                          ClipRRect(
+                            borderRadius:
+                            BorderRadius.all(Radius.circular(20)),
+                            child: FadeInImage(
+                              placeholder: MemoryImage(kTransparentImage),
+                              image: AssetImage(
+                                'Images/juhee1.jpg',
+                              ),
                               fit: BoxFit.fill,
+                              fadeInDuration: Duration(seconds: 1),
+                              fadeOutDuration: Duration(seconds: 1),
+                              fadeInCurve: Curves.easeIn,
+                              fadeOutCurve: Curves.easeIn,
+                              height: 40 * SizeConfig.widthMultiplier,
+                              width: 40 * SizeConfig.widthMultiplier,
+                              repeat: ImageRepeat.repeat,
                             ),
                           ),
-                        ),
+                        ],
                       ),
                     ),
                   ],
@@ -148,68 +165,83 @@ class _ChannelPageState extends State<ChannelPage> {
                   mainAxisAlignment: MainAxisAlignment.center,
                   crossAxisAlignment: CrossAxisAlignment.center,
                   children: <Widget>[
-                    Expanded(
-                      flex: 1,
-                      child: Padding(
-                        padding: EdgeInsets.fromLTRB(
-                          1 * SizeConfig.widthMultiplier,
-                          1 * SizeConfig.heightMultiplier,
-                          1 * SizeConfig.widthMultiplier,
-                          1 * SizeConfig.heightMultiplier,
-                        ),
-                        child: Container(
-                          width: 10 * SizeConfig.widthMultiplier,
-                          height: 30 * SizeConfig.heightMultiplier,
-                          decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(8.0),
-                              border: Border.all(color: Colors.grey[300]),
-                              boxShadow: [
-                                BoxShadow(
-                                    color: Colors.grey[300],
-                                    blurRadius: 1.0,
-                                    spreadRadius: 1.0,
-                                    offset: Offset(3.0, 3.0))
-                              ]),
-                          child: ClipRRect(
-                            borderRadius: BorderRadius.circular(8.0),
-                            child: Image.asset(
-                              'Images/juhee0.jpg',
+                    Padding(
+                      padding: EdgeInsets.fromLTRB(
+                        1 * SizeConfig.widthMultiplier,
+                        1 * SizeConfig.heightMultiplier,
+                        1 * SizeConfig.widthMultiplier,
+                        1 * SizeConfig.heightMultiplier,
+                      ),
+                      child: Stack(
+                        children: <Widget>[
+                          Padding(
+                            padding: EdgeInsets.fromLTRB(
+                              3 * SizeConfig.widthMultiplier,
+                              3 * SizeConfig.heightMultiplier,
+                              3 * SizeConfig.widthMultiplier,
+                              3 * SizeConfig.heightMultiplier,
+                            ),
+                            child: CircularProgressIndicator(),
+                          ),
+                          ClipRRect(
+                            borderRadius:
+                            BorderRadius.all(Radius.circular(20)),
+                            child: FadeInImage(
+                              placeholder: MemoryImage(kTransparentImage),
+                              image: AssetImage(
+                                'Images/juhee1.jpg',
+                              ),
                               fit: BoxFit.fill,
+                              fadeInDuration: Duration(seconds: 1),
+                              fadeOutDuration: Duration(seconds: 1),
+                              fadeInCurve: Curves.easeIn,
+                              fadeOutCurve: Curves.easeIn,
+                              height: 40 * SizeConfig.widthMultiplier,
+                              width: 40 * SizeConfig.widthMultiplier,
+                              repeat: ImageRepeat.repeat,
                             ),
                           ),
-                        ),
+                        ],
                       ),
                     ),
-                    Expanded(
-                      flex: 1,
-                      child: Padding(
-                        padding: EdgeInsets.fromLTRB(
-                          1 * SizeConfig.widthMultiplier,
-                          1 * SizeConfig.heightMultiplier,
-                          1 * SizeConfig.widthMultiplier,
-                          1 * SizeConfig.heightMultiplier,
-                        ),
-                        child: Container(
-                          width: 10 * SizeConfig.widthMultiplier,
-                          height: 30 * SizeConfig.heightMultiplier,
-                          decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(8.0),
-                              border: Border.all(color: Colors.grey[300]),
-                              boxShadow: [
-                                BoxShadow(
-                                    color: Colors.grey[300],
-                                    blurRadius: 1.0,
-                                    spreadRadius: 1.0,
-                                    offset: Offset(3.0, 3.0))
-                              ]),
-                          child: ClipRRect(
-                            borderRadius: BorderRadius.circular(8.0),
-                            child: Image.asset(
-                              'Images/juhee1.jpg',
+                    SizedBox(width: 3 * SizeConfig.widthMultiplier,),
+                    Padding(
+                      padding: EdgeInsets.fromLTRB(
+                        1 * SizeConfig.widthMultiplier,
+                        1 * SizeConfig.heightMultiplier,
+                        1 * SizeConfig.widthMultiplier,
+                        1 * SizeConfig.heightMultiplier,
+                      ),
+                      child: Stack(
+                        children: <Widget>[
+                          Padding(
+                            padding: EdgeInsets.fromLTRB(
+                              3 * SizeConfig.widthMultiplier,
+                              3 * SizeConfig.heightMultiplier,
+                              3 * SizeConfig.widthMultiplier,
+                              3 * SizeConfig.heightMultiplier,
+                            ),
+                            child: CircularProgressIndicator(),
+                          ),
+                          ClipRRect(
+                            borderRadius:
+                            BorderRadius.all(Radius.circular(20)),
+                            child: FadeInImage(
+                              placeholder: MemoryImage(kTransparentImage),
+                              image: AssetImage(
+                                'Images/juhee1.jpg',
+                              ),
                               fit: BoxFit.fill,
+                              fadeInDuration: Duration(seconds: 1),
+                              fadeOutDuration: Duration(seconds: 1),
+                              fadeInCurve: Curves.easeIn,
+                              fadeOutCurve: Curves.easeIn,
+                              height: 40 * SizeConfig.widthMultiplier,
+                              width: 40 * SizeConfig.widthMultiplier,
+                              repeat: ImageRepeat.repeat,
                             ),
                           ),
-                        ),
+                        ],
                       ),
                     ),
                   ],
@@ -251,7 +283,9 @@ class _ChannelPageState extends State<ChannelPage> {
                         child: Center(
                           child: Text(
                             '근처 이성 만나기',
-                            style: TextStyle(color: Colors.white, fontSize: 8 * SizeConfig.widthMultiplier),
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 8 * SizeConfig.widthMultiplier),
                           ),
                         ),
                       ),
@@ -287,7 +321,9 @@ class _ChannelPageState extends State<ChannelPage> {
                         child: Center(
                           child: Text(
                             '평가 좋은 이성 만나기',
-                            style: TextStyle(color: Colors.white, fontSize: 8 * SizeConfig.widthMultiplier),
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 8 * SizeConfig.widthMultiplier),
                           ),
                         ),
                       ),
@@ -323,7 +359,9 @@ class _ChannelPageState extends State<ChannelPage> {
                         child: Center(
                           child: Text(
                             '이상형 만나기',
-                            style: TextStyle(color: Colors.white, fontSize: 8 * SizeConfig.widthMultiplier),
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 8 * SizeConfig.widthMultiplier),
                           ),
                         ),
                       ),
